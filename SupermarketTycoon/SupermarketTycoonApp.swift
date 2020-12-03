a208592e9ae8762e36899b2d5138b9bb807a6248
George-J-E <george.elsham@elsham.com>
2020-12-03 13:18:55 +0000

    Initial Commit

---
+++ SupermarketTycoon/SupermarketTycoonApp.swift
@@ -0,0 +1,20 @@
+//
+//  SupermarketTycoonApp.swift
+//  SupermarketTycoon
+//
+//  Created by George Elsham on 03/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct SupermarketTycoonApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
